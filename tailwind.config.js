926b36f4450c81760e70c7ad2082cb1e0bb58e0d
AEDELSTAN <x18520204626@gmail.com>
2024-04-08 21:48:05 +0800

    Update

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+import type { Config } from 'tailwindcss'
+
+
+
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  // ...
+} satisfies Config;
